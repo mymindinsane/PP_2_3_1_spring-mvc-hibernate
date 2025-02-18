d6eb50c31e574252aee6912c6c81976da1b6a92e
mymindinsane <arturguletskiy21@gmail.com>
2025-02-18 15:38:00 +0300

    feat: implement deleteUser method in UserController.java

--- src/main/java/web/controller/UserController.java
+++ src/main/java/web/controller/UserController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 import web.Model.User;
 import web.Service.UserService;
@@ -31,15 +32,18 @@ public class UserController {
 
     @GetMapping("/adduser")
     public String addUser(@ModelAttribute("user") User user) {
-/*
-        return "redirect:/index";
-*/
         return "adduser";
     }
 
     @PostMapping("/adduser")
     public String addUserPOST(@ModelAttribute("user") User user) {
         userService.addUser(user);
+        return "redirect:/index";
+    }
+
+    @PostMapping("delete")
+    private String deleteUser(@RequestParam("id") long userId){
+        userService.deleteUser(userService.getUserById(userId));
         return "redirect:/index";
     }
 }
