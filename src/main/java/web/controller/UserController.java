59f9e7dd51017ca39cff8caa104abfd7f3c148d5
mymindinsane <arturguletskiy21@gmail.com>
2025-02-18 12:40:46 +0300

    fix: hibernate errors

--- src/main/java/web/controller/UserController.java
+++ src/main/java/web/controller/UserController.java
@@ -4,21 +4,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import web.Service.CarService;
 import web.Service.UserServiceImpl;
 import web.Service.UserService;
 
 @Controller
 public class UserController {
-    UserService carService = new UserServiceImpl();
-    @GetMapping("/cars")
-    public String listCars(@RequestParam(required = false) Integer count, Model model) {
-        if (count == null || count >= 5){
-            model.addAttribute("carsList",carService.getAllCars());
-        } else {
-            model.addAttribute("carsList",carService.limitCars(count));
-        }
-        return "cars.html";
-
+    UserService userService = new UserServiceImpl();
+    @GetMapping("/index")
+    public String listUsers() {
+        return "index.html";
     }
 }
